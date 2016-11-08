5e847f0a729b749bb60f5c57fb4a8af64704360b
Klas Mellbourn <klas@mellbourn.net>
2016-11-08 20:58:05 +0100

    importing xml2js now works fine

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
+import * as xml2js from 'xml2js';
+
 console.dir('hello world');
+
+var xml = "<root>Hello xml2js!</root>"
+xml2js.parseString(xml, function (err, result) {
+    console.dir(result);
+});
