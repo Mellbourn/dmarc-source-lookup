84c61c00b8d3a4501c04da5c07f00da30972702e
Klas Mellbourn <klas@mellbourn.net>
2016-11-08 23:25:16 +0100

    now accepts file name on command line. Improved error handling

--- index.ts
+++ index.ts
@@ -2,6 +2,12 @@ import * as fs from 'fs';
 import * as xml2js from 'xml2js';
 import * as dns from 'dns';
 
+const xmlFileName = process.argv[2];
+
+if (!xmlFileName) {
+    throw 'Must give file name on command line';
+}
+
 function handleRecord(record: any) {
     const row = record.row[0];
     const policy_evaluated = row.policy_evaluated[0];
@@ -9,24 +15,35 @@ function handleRecord(record: any) {
     const spf = policy_evaluated.spf[0];
     if (dkim === 'fail' && spf === 'fail') {
         const source_ip = row.source_ip[0];
-        console.log('failed source_ip', source_ip);
-        dns.reverse(source_ip, (err, hostnames) => {
-            if (err) {
-                console.warn('failed for', source_ip);
-            } else {
-                console.log('hostnames:', hostnames);
-            }
-        });
+        handleFail(source_ip);
     }
 }
 
-const parser = new xml2js.Parser();
-fs.readFile(__dirname + '/google.com!mellbourn.net!1478476800!1478563199.xml', function (err, data) {
-    parser.parseString(<any>data, function (err: any, parsedXml: any) {
-        const records: any[] = parsedXml.feedback.record;
-        for (const record of records) {
-            handleRecord(record);
+function handleFail(source_ip: string) {
+    console.log('failed source_ip', source_ip);
+    dns.reverse(source_ip, (err, hostnames) => {
+        if (err) {
+            console.warn('failed for', source_ip);
+        } else {
+            console.log('hostnames:', hostnames);
         }
-        console.log('Done');
     });
-});
+}
+
+function parseFile(xmlFileName: string) {
+    const parser = new xml2js.Parser();
+    fs.readFile(xmlFileName, function (err, data) {
+        if(err) {
+            throw `Could not read file "${xmlFileName}"`;
+        }
+        parser.parseString(<any>data, function (err: any, parsedXml: any) {
+            const records: any[] = parsedXml.feedback.record;
+            for (const record of records) {
+                handleRecord(record);
+            }
+            console.log('Done');
+        });
+    });
+}
+
+parseFile(xmlFileName);
