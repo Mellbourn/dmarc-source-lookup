3c093b3e6421bb4bb9e25eb93e1332b15708bd44
Klas Mellbourn <klas@mellbourn.net>
2016-11-08 22:12:24 +0100

    now finds failed source_ips

--- index.ts
+++ index.ts
@@ -1,12 +1,22 @@
+import * as fs from 'fs';
 import * as xml2js from 'xml2js';
 
-console.dir('hello world');
-
-var obj = { foo: "bar", baz: 42 };
-var copy = Object.assign({}, obj);
-console.log(copy);
-
-var xml = "<root>Hello xml2js!</root>"
-xml2js.parseString(xml, function (err, result) {
-    console.dir(result);
+const parser = new xml2js.Parser();
+fs.readFile(__dirname + '/google.com!mellbourn.net!1478476800!1478563199.xml', function(err, data) {
+    parser.parseString(<any>data, function (err :any, parsedXml: any) {
+        const records: any[] = parsedXml.feedback.record;
+        for(const record of records) {
+            const row = record.row[0];
+            const policy_evaluated = row.policy_evaluated[0];
+            const dkim = policy_evaluated.dkim[0];
+            const spf = policy_evaluated.spf[0];
+            if(dkim === 'fail' && spf === 'fail') {
+                const source_ip = row.source_ip[0];
+                console.log('failed source_ip', source_ip);
+            }
+        }
+        console.log('Done');
+    });
 });
+
+console.log('last line in script');
