e2eb75ec67cec6e0cb3b59daa07097111a9505c5
Klas Mellbourn <klas@mellbourn.net>
2016-11-08 21:38:39 +0100

    es2015 makes Object.assign work!

--- index.ts
+++ index.ts
@@ -2,6 +2,10 @@ import * as xml2js from 'xml2js';
 
 console.dir('hello world');
 
+var obj = { foo: "bar", baz: 42 };
+var copy = Object.assign({}, obj);
+console.log(copy);
+
 var xml = "<root>Hello xml2js!</root>"
 xml2js.parseString(xml, function (err, result) {
     console.dir(result);
